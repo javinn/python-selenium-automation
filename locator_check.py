8aa676b67ff60582ad726b62870ccbab06637b09
javinn <javid.mehdizade@gmail.com>
2021-01-27 14:53:41 -0500

    locator_check.py added

--- locator_check.py
+++ locator_check.py
@@ -19,4 +19,8 @@ chrome_instance = webdriver.Chrome()
 chrome_instance.get(
     'https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
 
-chrome_instance.find_element(By.XPATH, '//i [@class = "a-icon a-icon-logo"]')
+chrome_instance.maximize_window()
+
+chrome_instance.find_element(By.XPATH, '//i [contains(@class, "a-icon-logo")]')
+
+chrome_instance.quit()
